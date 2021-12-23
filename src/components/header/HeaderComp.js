fbaa778f3edd3d9dbe5e724b6b07936a8265ff7d
nhanlnt-96 <nhanlnt@hotmail.com>
2021-12-24 02:11:48 +0700

    [integrate-btn]: Complete integrate btn

--- src/components/header/HeaderComp.js
+++ src/components/header/HeaderComp.js
@@ -1,4 +1,7 @@
 import React, {useEffect, useState} from 'react';
+import {useDispatch, useSelector} from "react-redux";
+import {connect} from "../../redux/blockchain/blockchainActions";
+import { fetchData } from "../../redux/data/dataActions";
 import {Container, Nav, Navbar} from "react-bootstrap";
 import LogoComp from "../logo/LogoComp";
 
@@ -33,6 +36,103 @@ const headerMenu = [
 
 const HeaderComp = () => {
   const [offsetY, setOffsetY] = useState(0);
+  const dispatch = useDispatch();
+  const blockchain = useSelector((state) => state.blockchain);
+  const data = useSelector((state) => state.data);
+  const [claimingNft, setClaimingNft] = useState(false);
+  const [feedback, setFeedback] = useState(`Click buy to mint your NFT.`);
+  const [mintAmount, setMintAmount] = useState(1);
+  const [CONFIG, SET_CONFIG] = useState({
+    CONTRACT_ADDRESS: "",
+    SCAN_LINK: "",
+    NETWORK: {
+      NAME: "",
+      SYMBOL: "",
+      ID: 0,
+    },
+    NFT_NAME: "",
+    SYMBOL: "",
+    MAX_SUPPLY: 1,
+    WEI_COST: 0,
+    DISPLAY_COST: 0,
+    GAS_LIMIT: 0,
+    MARKETPLACE: "",
+    MARKETPLACE_LINK: "",
+    SHOW_BACKGROUND: false,
+  });
+  
+  const claimNFTs = () => {
+    let cost = CONFIG.WEI_COST;
+    let gasLimit = CONFIG.GAS_LIMIT;
+    let totalCostWei = String(cost * mintAmount);
+    let totalGasLimit = String(gasLimit * mintAmount);
+    console.log("Cost: ", totalCostWei);
+    console.log("Gas limit: ", totalGasLimit);
+    setFeedback(`Minting your ${CONFIG.NFT_NAME}...`);
+    setClaimingNft(true);
+    blockchain.smartContract.methods
+      .mint(mintAmount)
+      .send({
+        gasLimit: String(totalGasLimit),
+        to: CONFIG.CONTRACT_ADDRESS,
+        from: blockchain.account,
+        value: totalCostWei,
+      })
+      .once("error", (err) => {
+        console.log(err);
+        setFeedback("Sorry, something went wrong please try again later.");
+        setClaimingNft(false);
+      })
+      .then((receipt) => {
+        console.log(receipt);
+        setFeedback(
+          `WOW, the ${CONFIG.NFT_NAME} is yours! go visit Opensea.io to view it.`
+        );
+        setClaimingNft(false);
+        dispatch(fetchData(blockchain.account));
+      });
+  };
+  
+  const decrementMintAmount = () => {
+    let newMintAmount = mintAmount - 1;
+    if (newMintAmount < 1) {
+      newMintAmount = 1;
+    }
+    setMintAmount(newMintAmount);
+  };
+  
+  const incrementMintAmount = () => {
+    let newMintAmount = mintAmount + 1;
+    if (newMintAmount > 10) {
+      newMintAmount = 10;
+    }
+    setMintAmount(newMintAmount);
+  };
+  
+  const getData = () => {
+    if (blockchain.account !== "" && blockchain.smartContract !== null) {
+      dispatch(fetchData(blockchain.account));
+    }
+  };
+  
+  const getConfig = async () => {
+    const configResponse = await fetch("/config/config.json", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    });
+    const config = await configResponse.json();
+    SET_CONFIG(config);
+  };
+  
+  useEffect(() => {
+    getConfig();
+  }, []);
+  
+  useEffect(() => {
+    getData();
+  }, [blockchain.account]);
   useEffect(() => {
     window.onscroll = () => {
       setOffsetY(window.pageYOffset);
@@ -56,7 +156,12 @@ const HeaderComp = () => {
             }
           </Nav>
           <Nav className="justify-content-center align-items-center header-comp-right-custom">
-            <button className="connect-btn">Connect wallet</button>
+            <button className="connect-btn" onClick={(e) => {
+              e.preventDefault();
+              dispatch(connect());
+              getData();
+            }}>Connect wallet
+            </button>
           </Nav>
         </Navbar.Collapse>
       </Container>
