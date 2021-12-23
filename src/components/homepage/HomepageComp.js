aff36a555405d9da0b0c4d4495f739645d9d6fdf
nhanlnt-96 <nhanlnt@hotmail.com>
2021-12-23 14:14:50 +0700

    [homepage]: Working on title

--- src/components/homepage/HomepageComp.js
+++ src/components/homepage/HomepageComp.js
@@ -10,6 +10,10 @@ const HomepageComp = () => {
       <Row>
         <HeaderComp/>
       </Row>
+      <Row className="homepage-comp-content">
+        <h1 className="content-title">"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur,
+          adipisci velit..."</h1>
+      </Row>
     </Container>
   );
 };
